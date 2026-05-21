1f7e8ad3cb6433b12e8078bec212a1763a1317c4
Frida Carbajal Ordóñez <fridacarbajalordonez@gmail.com>
2026-05-21 11:14:01 -0600

    Enhance database creation script with existence check
    Added checks to drop existing database before creation.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,13 +4,31 @@ CREATE DATABASE DataWarehouse and SCHEMAS bronze, silver, gold
 -*-*-*-*-*-*-*--*-*-*-*-*-*-*--*-*-*-*-*-*-*--*-*-*-*-*-*-*-
 
 Script purpose:
-SQL code to create a database named DataWarehouse and three schemas named bronze, silver, and gold.
-
+Before creating a database, it is important to check if a database with the same name already exists. 
+If it does, we will drop it before creating a new one. 
+Main purpose is to create a database named DataWarehouse and three schemas named bronze, silver, and gold.
 */
+
+
+USE  master;
+go
+ 
+--Checking if a exist a database named "DataWarehouse" before creating one
+IF  EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+go
+
+
+--Creating the database named "DataWarehouse"
 CREATE DATABASE DataWarehouse;
 
 USE DataWarehouse;
 
+
+-- Creating the schemas named bronze, silver, and gold
 CREATE SCHEMA bronze;
 go
 CREATE SCHEMA silver;
