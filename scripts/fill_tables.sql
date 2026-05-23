806b57b1b5c14dc05cae45b1278c010f0926aa8d
Frida Carbajal Ordóñez <fridacarbajalordonez@gmail.com>
2026-05-23 11:23:49 -0600

    Enhance data loading procedure with print statements
    The procedure now includes print statements for better tracking of the loading process and error handling.

--- scripts/fill_tables.sql
+++ scripts/fill_tables.sql
@@ -1,5 +1,4 @@
 
-
 /*
 -*-*-*-*-*-*-*--*-*-*-*-*-*-*--*-*-*-*-*-*-*--*-*-*-*-*-*-*-
 CREATES AN STORED PROCEDURE FOR LOADING DATA INTO BRONZE SCHEMA TABLES FROM CSV FILES 
@@ -8,59 +7,91 @@ CREATES AN STORED PROCEDURE FOR LOADING DATA INTO BRONZE SCHEMA TABLES FROM CSV 
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze as
 	BEGIN
+	BEGIN TRY
 
-	TRUNCATE TABLE bronze.crm_cust_info;
-	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK--Block the table during the bulk insert operation for improved performance
-	);
+		PRINT('======================================================================')
+		PRINT('LOADING DATA INTO BRONZE SCHEMA TABLES FROM CSV FILES')
+		PRINT('======================================================================')
 
-	TRUNCATE TABLE bronze.crm_prd_info;
-	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT('----------------------------------------------------------------------')
+		PRINT('Loading CRM tables')
+		PRINT('----------------------------------------------------------------------')
 
-	TRUNCATE TABLE bronze.crm_sales_details;
-	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
 
-	TRUNCATE TABLE bronze.erp_cust_az12;
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+	
+		PRINT('>>>>>Tuncating and inserting data into tables:')
+		PRINT( 'TABLE: bronze.crm_cust_info')
+		TRUNCATE TABLE bronze.crm_cust_info;
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK--Block the table during the bulk insert operation for improved performance
+		);
+		PRINT(' ')
 
-	TRUNCATE TABLE bronze.erp_loc_a101;
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT( 'TABLE: bronze.crm_prd_info')
+		TRUNCATE TABLE bronze.crm_prd_info;
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT(' ')
 
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT( 'TABLE: bronze.crm_sales_details')
+		TRUNCATE TABLE bronze.crm_sales_details;
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT(' ')
+	
+		PRINT('----------------------------------------------------------------------')
+		PRINT('Loading ERP tables')
+		PRINT('----------------------------------------------------------------------')
 
-END
+		PRINT('>>>>>Tuncating and inserting data into tables:')
+		PRINT( 'TABLE: bronze.erp_cust_az12')
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT(' ')
+
+		PRINT('TABLE: bronze.erp_loc_a101')
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT(' ')
+
+		PRINT('TABLE: bronze.erp_px_cat_g1v2')
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\secFc\OneDrive\Documentos\SQL\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+	END TRY
+	BEGIN CATCH
+		PRINT('An error occurred while loading data into bronze tables: ' + ERROR_MESSAGE());
+	END CATCH
+	END
+
